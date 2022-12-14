b87b044c8d7e37aedcc025b0d02ca2a6b338a73b
Haziq Dzulkafli <haziqdzulkafli@gmail.com>
2022-12-14 20:30:54 +0800

    language and some font size fixed

--- lib/widgets/expanded_widged.dart
+++ lib/widgets/expanded_widged.dart
@@ -71,23 +71,23 @@ class _ExpandedWidgetState extends State<ExpandedWidget> {
                         ),
                       ),
                     ],
-                  )
-                : Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        widget.title,
-                        style: textStyleBoldSmall,
-                        textAlign: TextAlign.center,
-                      ),
-                      Text(
-                        widget.text,
-                        style: textStyleNormal,
-                        textAlign: TextAlign.justify,
-                      ),
-                    ],
                   ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  widget.title,
+                  style: textStyleBoldSmall,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  widget.text,
+                  style: textStyleNormal,
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
             const SizedBox(height: 15),
             Align(
                 alignment: Alignment.bottomRight,
